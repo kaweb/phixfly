5fdbf677eb757876c2e3bd0594c385f0ed5b9502
b3none <ablackham2000@gmail.com>
2018-09-17 15:34:24 +0100

    Switched to using var_dump for the example.

--- example.php
+++ example.php
@@ -5,4 +5,4 @@ include('vendor/autoload.php');
 $Phixfly = \Kaweb\Phixfly\Client::create('yoursubdomain', 'th15154nex4mpl3t0k3n');
 $response = $Phixfly->contractorEndpoints()->getContractorServices();
 
-print_r($response);
+var_dump($response);
