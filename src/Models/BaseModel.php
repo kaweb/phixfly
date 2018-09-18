4a7fabe2fc35bbe44203302a98d1cef91cdb9246
b3none <ablackham2000@gmail.com>
2018-09-18 09:19:41 +0100

    We now catch ReflectionExceptions by returning an empty array.

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -9,21 +9,24 @@ class BaseModel
      * This function will allow us to get the object's protected variables as an array.
      *
      * @return array
-     * @throws \ReflectionException
      */
     function toArray()
     {
-        $backtrace = debug_backtrace();
+        try {
+            $backtrace = debug_backtrace();
 
-        $reflection = new \ReflectionClass(get_class($backtrace[sizeof(0) - 1]['object']));
-        $protectedProperties = $reflection->getProperties(\ReflectionProperty::IS_PROTECTED);
+            $reflection = new \ReflectionClass(get_class($backtrace[sizeof(0) - 1]['object']));
+            $protectedProperties = $reflection->getProperties(\ReflectionProperty::IS_PROTECTED);
 
-        $objectParameters = [];
-        foreach ($protectedProperties as $protectedProperty) {
-            $protectedProperty->setAccessible(true);
-            $objectParameters[$protectedProperty->getName()] = $protectedProperty->getValue($this);
+            $objectParameters = [];
+            foreach ($protectedProperties as $protectedProperty) {
+                $protectedProperty->setAccessible(true);
+                $objectParameters[$protectedProperty->getName()] = $protectedProperty->getValue($this);
+            }
+
+            return $objectParameters;
+        } catch (\ReflectionException $exception) {
+            return [];
         }
-
-        return $objectParameters;
     }
 }
