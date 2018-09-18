a6380ba64384c57ad54b91f3e22b3f26a4224ec1
b3none <ablackham2000@gmail.com>
2018-09-18 09:36:46 +0100

    Added QuickViewPanelEndpoints.

--- src/Categories/QuickViewPanelEndpoints.php
+++ src/Categories/QuickViewPanelEndpoints.php
@@ -20,4 +20,69 @@ class QuickViewPanelEndpoints
     {
         $this->requestHelper = $requestHelper;
     }
+
+    /**
+     * http://api-docs.fixflo.com/#v2-endpoints-v2-endpoints-quick-view-panel-endpoints-get-quick-view-panel
+     *
+     * @return array
+     */
+    public function getQuickViewPanel()
+    {
+        return $this->requestHelper->get('/qvp');
+    }
+
+    /**
+     * http://api-docs.fixflo.com/#v2-endpoints-v2-endpoints-quick-view-panel-endpoints-qvpcalendareventlist
+     *
+     * @param int $id
+     * @return array
+     */
+    public function QVPCalendarEventList($id)
+    {
+        return $this->requestHelper->get('/qvp/calendareventpmtasklist/' . $id);
+    }
+
+    /**
+     * http://api-docs.fixflo.com/#v2-endpoints-v2-endpoints-quick-view-panel-endpoints-qvpissuestatussummary
+     *
+     * @param int $id
+     * @return array
+     */
+    public function QVPIssueStatusSummary($id)
+    {
+        return $this->requestHelper->get('/qvp/issuestatussummary/' . $id);
+    }
+
+    /**
+     * http://api-docs.fixflo.com/#v2-endpoints-v2-endpoints-quick-view-panel-endpoints-qvpissuesummary
+     *
+     * @param int $id
+     * @return array
+     */
+    public function QVPIssueSummary($id)
+    {
+        return $this->requestHelper->get('/qvp/issuesummary/' . $id);
+    }
+
+    /**
+     * http://api-docs.fixflo.com/#v2-endpoints-v2-endpoints-quick-view-panel-endpoints-qvpkeylabelvalues
+     *
+     * @param int $id
+     * @return array
+     */
+    public function QVPKeyLabelValues($id)
+    {
+        return $this->requestHelper->get('/qvp/keylabelvalues/' . $id);
+    }
+
+    /**
+     * http://api-docs.fixflo.com/#v2-endpoints-v2-endpoints-quick-view-panel-endpoints-qvpkvpairs
+     *
+     * @param int $id
+     * @return array
+     */
+    public function QVPKVPairs($id)
+    {
+        return $this->requestHelper->get('/qvp/kvpairs/' . $id);
+    }
 }
